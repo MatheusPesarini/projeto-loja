d2ea43b8df18720ed5d29e4a71f1bfe3cf6085ff
Matheus <mrogeriopesarini@gmail.com>
2025-04-23 19:06:30 -0300

    refatorar: ajustar a formatação do layout de login e registro, e adicionar novo componente de rodapé

--- frontend-next/src/app/(public)/register/page.tsx
+++ frontend-next/src/app/(public)/register/page.tsx
@@ -4,7 +4,7 @@ import RegisterForm from "@/components/register-form";
 
 export default function Page() {
 	return (
-		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+		<div className="flex w-full justify-center p-6 md:p-28">
 			<div className="w-full max-w-sm">
 				<RegisterForm />
 			</div>
