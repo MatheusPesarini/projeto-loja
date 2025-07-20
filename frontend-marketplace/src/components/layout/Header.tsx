aea69b1b9210b5b344927b40110c3b5260974b7e
Matheus <mrogeriopesarini@gmail.com>
2025-07-20 16:26:26 -0300

    White Theme rework

--- frontend-marketplace/src/components/layout/Header.tsx
+++ frontend-marketplace/src/components/layout/Header.tsx
@@ -190,55 +190,53 @@ export default function Navbar({
 					<div className="flex items-center justify-self-end gap-2">
 						<InputSearch className="w-48 h-8" placeholder="Procurar" />
 						{isAuthenticated ? (
-							<>
-								<DropdownMenu>
-									<DropdownMenuTrigger asChild>
-										<Image
-											src="https://avatar.vercel.sh/default"
-											alt="Avatar"
-											width={36}
-											height={36}
-											className="rounded-full cursor-pointer"
-										/>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent
-										className="w-48"
-										align="end"
-										sideOffset={5}
-										avoidCollisions={true}
-										side="bottom"
-										sticky="always"
-									>
-										<DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
+							<DropdownMenu>
+								<DropdownMenuTrigger asChild>
+									<Image
+										src="https://avatar.vercel.sh/default"
+										alt="Avatar"
+										width={36}
+										height={36}
+										className="rounded-full cursor-pointer"
+									/>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent
+									className="w-48"
+									align="end"
+									sideOffset={5}
+									avoidCollisions={true}
+									side="bottom"
+									sticky="always"
+								>
+									<DropdownMenuLabel>Minha Conta</DropdownMenuLabel>
+									<DropdownMenuSeparator />
+									<DropdownMenuGroup>
+										<DropdownMenuItem asChild>
+											<Link
+												href="/profile"
+												className="flex items-center cursor-pointer"
+											>
+												<User className="mr-2 h-4 w-4" />
+												<span>Perfil</span>
+											</Link>
+										</DropdownMenuItem>
+										<DropdownMenuItem asChild>
+											<Link
+												href="/orders"
+												className="flex items-center cursor-pointer"
+											>
+												<Store className="mr-2 h-4 w-4" />
+												<span>Meus Pedidos</span>
+											</Link>
+										</DropdownMenuItem>
 										<DropdownMenuSeparator />
-										<DropdownMenuGroup>
-											<DropdownMenuItem asChild>
-												<Link
-													href="/profile"
-													className="flex items-center cursor-pointer"
-												>
-													<User className="mr-2 h-4 w-4" />
-													<span>Perfil</span>
-												</Link>
-											</DropdownMenuItem>
-											<DropdownMenuItem asChild>
-												<Link
-													href="/orders"
-													className="flex items-center cursor-pointer"
-												>
-													<Store className="mr-2 h-4 w-4" />
-													<span>Meus Pedidos</span>
-												</Link>
-											</DropdownMenuItem>
-											<DropdownMenuSeparator />
-											<DropdownMenuItem className="cursor-pointer ">
-												<LogOut className="mr-2 h-4 w-4" />
-												<LogoutButton />
-											</DropdownMenuItem>
-										</DropdownMenuGroup>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</>
+										<DropdownMenuItem className="cursor-pointer ">
+											<LogOut className="mr-2 h-4 w-4" />
+											<LogoutButton />
+										</DropdownMenuItem>
+									</DropdownMenuGroup>
+								</DropdownMenuContent>
+							</DropdownMenu>
 						) : (
 							<>
 								<Button asChild variant="outline" size="sm">
@@ -284,9 +282,7 @@ export default function Navbar({
 
 									<div className="flex flex-col gap-3">
 										{isAuthenticated ? (
-											<>
-												<LogoutButton />
-											</>
+											<LogoutButton />
 										) : (
 											<>
 												<Button asChild variant="outline" size="sm">
